cdc4db5325821fc3422d155ec160c4ef4f3e47be
Mazin Zain <zid.m@pm.me>
2022-03-19 00:27:51 +0000

    Upload New File

---
+++ lang_en.py
@@ -0,0 +1,23 @@
+from APP_INFO import *
+
+lang_error_title = SW_NAME + "can't run on your system"
+lang_error_uefi_0 = 'Your system does not support (or is not using) UEFI boot.'
+lang_error_uefi_9 = "UEFI boot support could not be verified."
+lang_error_totalram_0 = "Your system does not have sufficient RAM capacity."
+lang_error_totalram_9 = "Failed to check available RAM capacity."
+lang_error_space_0 = "Not enough space on your system drive. Free up some space and try again later"
+lang_error_space_9 = "Failed to check available disk space on your system drive."
+lang_error_resizable_0 = "Failed to check system drive resizability."
+lang_error_resizable_9 = "Not enough shrink room on system drive."
+
+
+lang_install_question = "How do you wanna install " + distro_name + "?"
+lang_install_option1 = "Quick Install with KDE Desktop"
+lang_install_option2 = "Quick Install with GNOME Desktop"
+lang_install_option3 = "Advanced: Let me configure my applets later"
+
+lang_windows_question = "Got it, what about Windows and your data?"
+lang_windows_option1 = "Remove Windows & migrate my Library (Music, Photos, Videos) to Linux"
+lang_windows_option2 = "Nuke Windows & all data(!) and start fresh with Linux"
+lang_windows_option3 = "Do nothing and let me partition later (Dual boot possible)"
+
