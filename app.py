5998b1587cd5f7c4b89bcf339c799e49034d7d48
Mazin Zain <zid.m@pm.me>
2022-03-19 00:27:16 +0000

    Replace app.py

--- app.py
+++ app.py
@@ -1,135 +1,68 @@
+from APP_INFO import *
 import tkinter as tk
 from tkinter import ttk
-import webbrowser
-import subprocess
-import powershell
-import concurrent.futures
+from multiprocessing import Process
+from lang_en import *
+from functions import *
+
+# Driver Code
+app = tk.Tk()
+app.title(SW_NAME)
+app.geometry("800x500")
+# app.iconbitmap("yourimage.ico")
+app.resizable(False, False)
+app.tk.call('tk', 'scaling', 1.5)
+win_width, win_height = 550, 400
 
 LARGEFONT = ("Ariel", 20)
 MEDIUMFONT = ("Ariel", 16)
 SMALLFONT = ("Ariel", 12)
 
+# creating a container
+container = tk.Frame(app)
+container.pack(side="top", fill="both", expand=True)
+container.grid_rowconfigure(0, weight=1)
+container.grid_columnconfigure(0, weight=1)
 
-class TkinterApp(tk.Tk):
-
-    # __init__ function for class tkinterApp
-    def __init__(self, *args, **kwargs):
-        # __init__ function for class Tk
-        tk.Tk.__init__(self, *args, **kwargs)
-
-        # creating a container
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        # initializing frames to an empty array
-        self.frames = {}
-
-        # iterating through a tuple consisting
-        # of the different page layouts
-        for F in (PageCheck, PageError, Page1, Page2):
-            frame = F(container, self)
-
-            # initializing frame of that object from
-            # startpage, page1, page2 respectively with
-            # for loop
-            self.frames[F] = frame
-
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(PageCheck)
-
-    # to display the current frame passed as
-    # parameter
-    def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
+top_frame = tk.Frame(container, height=100, bg='yellow')
+top_frame.pack(fill="x", ipadx=10, ipady=10)
+left_frame = tk.Frame(container, width=200, bg='blue')
+left_frame.pack(fill="y", side="left", ipadx=10, ipady=10)
+middle_frame = tk.Frame(container, bg='cyan', height=800)
+middle_frame.pack(fill="both", expand=1, padx=20, pady=20)
 
 
-# first window frame startpage
-
-class StartPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-        # label of frame Layout 2
-        top_frame = tk.Frame(self, height=100, bg='yellow')
-        top_frame.pack(fill="x", ipadx=10, ipady=10)
-
-        left_frame = tk.Frame(self, width=200, bg='blue')
-        left_frame.pack(fill="y", side="left", ipadx=10, ipady=10)
-
-        middle_frame = tk.Frame(self, bg='cyan', height=800)
-        middle_frame.pack(fill="both", expand=1, padx=20, pady=20)
-
-        def open_url(url):
-            webbrowser.open_new_tab(url)
-
-        # def start_test():
-        #    p1 = controller.show_frame(PageCheck)
-        #    p2 = PageCheck.compatibility_test(self)
-        #    p1.start()
-        #    p2.start()
-        # Top Frame Content
-        # img_app_logo = tk.PhotoImage(file="appLogo.png")
-        # btn_app_logo = tk.Label(top_frame, image=img_app_logo, cursor="hand2")
-        # url1 = 'https://www.kernel.org/'
-        # btn_app_logo.pack(side="left")
-        # btn_app_logo.bind("<Button-1>", lambda e: open_url(url1))
-
-        # text_moto = tk.Label(top_frame, text="Free as in Freedom")
-        # text_moto.pack(side="left")
-
-        # img_suse = tk.PhotoImage(file="Suse-logo.png")
-        # btn_suse = tk.Label(top_frame, image=img_suse, cursor="hand2")
-        # url2 = 'https://www.opensuse.org/'
-        # btn_suse.pack(side="right")
-        # btn_suse.bind("<Button-1>", lambda e: open_url(url2))
-
-        label = ttk.Label(middle_frame, wraplength=win_width,
-                          text="Hi, this is Lnixify, the fastest way to deploy GNU/Linux from a Windows machine",
-                          font=LARGEFONT)
-        label.pack(anchor="w")
-        label2 = ttk.Label(middle_frame, wraplength=win_width,
-                           text="Click Check to test your system compatibility", font=MEDIUMFONT)
-        label2.pack(pady=40, anchor="w")
-        button1 = ttk.Button(middle_frame, text="Check ", style="big.TButton",
-                             command=lambda: [controller.show_frame(PageCheck), PageCheck.compatibility_test(self)])
-
-        button1.pack(anchor="se", side="right", ipadx=15, padx=10)
+def clear_frame():
+    for widgets in middle_frame.winfo_children():
+        widgets.destroy()
 
 
-class PageCheck(tk.Frame):
+selections = {}
 
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
 
-        top_frame = tk.Frame(self, height=100, bg='yellow')
-        top_frame.pack(fill="x", ipadx=10, ipady=10)
-
-        left_frame = tk.Frame(self, width=200, bg='blue')
-        left_frame.pack(fill="y", side="left", ipadx=10, ipady=10)
-
-        middle_frame = tk.Frame(self, bg='cyan', height=800)
-        middle_frame.pack(fill="both", expand=1, padx=20, pady=20)
+# page_check
+def main():
+    def page_check():
+        page_name = "page_check"
 
         label2 = ttk.Label(middle_frame, text="Checking System requirements. please wait...", font=MEDIUMFONT)
         label3 = ttk.Label(middle_frame, text="Compatibility Check Passed!", font=MEDIUMFONT)
-        label4 = ttk.Label(middle_frame, text="Compatibility Check Failed!", font=MEDIUMFONT)
 
-        pb_hD = ttk.Progressbar(middle_frame, orient='horizontal', length=500, mode='indeterminate')
+        proggresbar_check = ttk.Progressbar(middle_frame, orient='horizontal', length=500, mode='indeterminate')
 
         label2.pack(pady=40, anchor="w")
-        pb_hD.pack(expand=True)
+        proggresbar_check.pack(expand=True)
 
-        pb_hD.start(10)
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            future = executor.submit(self.compatibility_test)
-            compatibility_results = future.result()
+        proggresbar_check.start(10)
+        queue1 = Queue()
+
+        p1 = Process(target=compatibility_test, args=(queue1,))
+        p1.start()
+        while queue1.empty():
+            app.update()
+        compatibility_results = queue1.get()
         button1 = ttk.Button(middle_frame, text="Next",
-                             command=lambda: controller.show_frame(Page1))
+                             command=lambda: page_1())
 
         button2 = ttk.Button(middle_frame, text="Quit",
                              command=lambda: app.destroy())
@@ -137,250 +70,95 @@ class PageCheck(tk.Frame):
         print(compatibility_results)
         if compatibility_results['result_uefi_check'] == 1 and compatibility_results['result_totalram_check'] == 1 and \
                 compatibility_results['result_space_check'] in (1, 2):
-            label2.pack_forget()
-            pb_hD.pack_forget()
-            label3.pack(pady=40, anchor="nw")
-            button1.pack(anchor="se", side="right", ipadx=15, padx=10)
-            button2.pack(anchor="se", side="right", ipadx=15, padx=10)
+            page_1()
         else:
-            label2.pack_forget()
-            pb_hD.pack_forget()
-            label4.pack(pady=40, anchor="nw")
-            errors = []
 
+            label2.pack_forget()
+            proggresbar_check.pack_forget()
+            label4 = ttk.Label(middle_frame, text=lang_error_title, font=MEDIUMFONT)
+            errors = []
             if compatibility_results['result_uefi_check'] == 0:
-                errors.append('Your system does not support (or is not using) UEFI boot.')
+                errors.append(lang_error_uefi_0)
             if compatibility_results['result_uefi_check'] == 9:
-                errors.append("UEFI boot support could not be verified.")
+                errors.append(lang_error_uefi_9)
             if compatibility_results['result_totalram_check'] == 0:
-                errors.append("Your system does not have sufficient RAM capacity.")
+                errors.append(lang_error_totalram_0)
             if compatibility_results['result_totalram_check'] == 9:
-                errors.append("Failed to check available RAM capacity.")
+                errors.append(lang_error_totalram_9)
             if compatibility_results['result_space_check'] == 0:
-                errors.append("Not enough disk space on your system drive.")
+                errors.append(lang_error_space_0)
             if compatibility_results['result_space_check'] == 9:
-                errors.append("Failed to check available disk space on your system drive.")
+                errors.append(lang_error_space_9)
             if compatibility_results['result_resizable_check'] == 9:
-                errors.append("Failed to check system drive resizability.")
+                errors.append(lang_error_resizable_0)
             if compatibility_results['result_resizable_check'] == 0:
-                errors.append("Not enough shrink room on system drive.")
+                errors.append(lang_error_resizable_9)
 
             label5 = ttk.Label(middle_frame, text=("\n".join(errors)), font=SMALLFONT)
+
+            label4.pack(pady=40, anchor="nw")
             label5.pack(padx=10, anchor="w")
             button2.pack(anchor="se", side="right", ipadx=15, padx=10)
 
-    def compatibility_test(self):
+    # page_1
+    def page_1():
+        page_name = "page_1"
+        clear_frame()
 
-        print('hey')
-        required_space_min = 900 * 1024 * 1024
-        required_space_dualboot = required_space_min + 70 * 1024 * 1024 * 1024
-        required_ram = 4
-
-        def check_uefi():
-            return subprocess.run([r'powershell.exe', r'$env:firmware_type'], stdout=subprocess.PIPE,
-                                  stderr=subprocess.STDOUT, shell=True)
-
-        def check_totalram():
-            return subprocess.run([r'powershell.exe',
-                                   r'(Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb'],
-                                  stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-
-        def check_space():
-            return subprocess.run([r'powershell.exe',
-                                   r'(Get-Volume | Where DriveLetter -eq $env:SystemDrive.Substring(0, 1)).SizeRemaining'],
-                                  stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-
-        def check_resizable():
-            return subprocess.run([r'powershell.exe',
-                                   r'((Get-Volume | Where DriveLetter -eq $env:SystemDrive.Substring(0, 1)).Size - (Get-PartitionSupportedSize -DriveLetter $env:SystemDrive.Substring(0, 1)).SizeMin)'],
-                                  stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-
-        totalram = int(check_totalram().stdout)
-        result_resizable_check = 0
-        if check_totalram().returncode != 0:
-            result_totalram_check = 9
-        elif totalram >= required_ram:
-            result_totalram_check = 1
-        else:
-            result_totalram_check = 0
-        if check_uefi().returncode != 0:
-            result_uefi_check = 9
-        elif b'uefi' in check_uefi().stdout.lower():
-            result_uefi_check = 1
-        else:
-            result_uefi_check = 0
-
-        if check_space().returncode != 0:
-            result_space_check = 9
-
-        space_available = int(str(check_space().stdout)[2:-5])
-
-        if space_available > required_space_dualboot:
-            result_space_check = 2
-        elif space_available > required_space_min:
-            result_space_check = 1
-        else:
-            result_space_check = 0
-
-        if result_space_check in (1, 2):
-            if check_resizable().returncode != 0:
-                result_resizable_check = 9
-            elif int(str(check_resizable().stdout)[2:-5]) > required_space_min:
-                result_resizable_check = 1
-            else:
-                result_resizable_check = 0
-        else:
-            result_resizable_check = 8
-        check_results = {'result_uefi_check': result_uefi_check,
-                         'result_totalram_check': result_totalram_check,
-                         'result_space_check': result_space_check,
-                         'result_resizable_check': result_resizable_check}
-        return check_results
-
-
-class Page1(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-        top_frame = tk.Frame(self, height=100, bg='yellow')
-        top_frame.pack(fill="x", ipadx=10, ipady=10)
-
-        left_frame = tk.Frame(self, width=200, bg='blue')
-        left_frame.pack(fill="y", side="left", ipadx=10, ipady=10)
-
-        middle_frame = tk.Frame(self, bg='cyan', height=800)
-        middle_frame.pack(fill="both", expand=1, padx=20, pady=20)
-
-        # button1 = ttk.Button(self, text="StartPage",
-        #                     command=lambda: controller.show_frame(StartPage))
-        # button1.pack()
-
-        # button to show frame 2 with text
-        # layout2
-        # label = ttk.Label(middle_frame, text="First, help me help you", font=LARGEFONT)
-        # label.pack(anchor="w")
-        label2 = ttk.Label(middle_frame, text="Which option describes you the best?", font=MEDIUMFONT)
+        label2 = ttk.Label(middle_frame, text=lang_install_question, font=MEDIUMFONT)
         label2.pack(pady=40, anchor="w")
-        button1 = ttk.Button(middle_frame, text="Next", style="big.TButton",
-                             command=lambda: controller.show_frame(Page2))
-
+        button1 = ttk.Button(middle_frame, text="Next",
+                             command=lambda: page_2())
         # putting the button in its place by
         # using grid
-
-        var = tk.StringVar(self, "1")
-
+        var1 = tk.IntVar(app, 1)
         # Dictionary to create multiple buttons
-        values = {"Quick Install with KDE Desktop (Choose this if your new to Linux)": "1",
-                  "Quick Install with GNOME Desktop": "2",
-                  "Advanced: Let me configure my applets later": "3"}
+        values = {lang_install_option1: 1,
+                  lang_install_option2: 2,
+                  lang_install_option3: 3}
 
         # Loop is used to create multiple Radiobuttons
         # rather than creating each button separately
+        var1.set(1)
         for (text, value) in values.items():
-            ttk.Radiobutton(middle_frame, text=text, variable=var,
+            ttk.Radiobutton(middle_frame, text=text, variable=var1,
                             value=value).pack(anchor="w", ipady=5)
-        var.set("1")
 
         button1.pack(anchor="se", side="right", ipadx=15, padx=10)
 
-
-class PageError(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-        top_frame = tk.Frame(self, height=100, bg='yellow')
-        top_frame.pack(fill="x", ipadx=10, ipady=10)
-
-        left_frame = tk.Frame(self, width=200, bg='blue')
-        left_frame.pack(fill="y", side="left", ipadx=10, ipady=10)
-
-        middle_frame = tk.Frame(self, bg='cyan', height=800)
-        middle_frame.pack(fill="both", expand=1, padx=20, pady=20)
-
-        # button1 = ttk.Button(self, text="StartPage",
-        #                     command=lambda: controller.show_frame(StartPage))
-        # button1.pack()
-
-        # button to show frame 2 with text
-        # layout2
-        # label = ttk.Label(middle_frame, text="First, help me help you", font=LARGEFONT)
-        # label.pack(anchor="w")
-        label2 = ttk.Label(middle_frame, text="Could not complete compatibility test", font=MEDIUMFONT)
-        label2.pack(pady=40, anchor="w")
-        button1 = ttk.Button(middle_frame, text="Next", style="big.TButton",
-                             command=lambda: controller.show_frame(Page2))
-
-        # putting the button in its place by
-        # using grid
-
-        var = tk.StringVar(self, "1")
-
-        # Dictionary to create multiple buttons
-        values = {"Quick Install with KDE Desktop (Choose this if your new to Linux)": "1",
-                  "Quick Install with GNOME Desktop": "2",
-                  "Advanced: Let me configure my applets later": "3"}
-
-        # Loop is used to create multiple Radiobuttons
-        # rather than creating each button separately
-        for (text, value) in values.items():
-            ttk.Radiobutton(middle_frame, text=text, variable=var,
-                            value=value).pack(anchor="w", ipady=5)
-        var.set("1")
-
-        button1.pack(anchor="se", side="right", ipadx=15, padx=10)
-
-
-# third window frame page2
-class Page2(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-        top_frame = tk.Frame(self, height=100, bg='yellow')
-        top_frame.pack(fill="x", ipadx=10, ipady=10)
-
-        left_frame = tk.Frame(self, width=200, bg='blue')
-        left_frame.pack(fill="y", side="left", ipadx=10, ipady=10)
-
-        middle_frame = tk.Frame(self, bg='cyan', height=800)
-        middle_frame.pack(fill="both", expand=1, padx=20, pady=20)
-
-        label2 = ttk.Label(middle_frame, text="Got it, what do you want me to do with Windows?", font=MEDIUMFONT)
+    # page_2
+    def page_2():
+        page_name = "page_2"
+        clear_frame()
+        label2 = ttk.Label(middle_frame, text=lang_windows_question, font=MEDIUMFONT)
         label2.pack(pady=40, anchor="w")
 
         button1 = ttk.Button(middle_frame, text="Next",
-                             command=lambda: controller.show_frame(Page1))
+                             command=lambda: page_1())
 
         button2 = ttk.Button(middle_frame, text="Back",
-                             command=lambda: controller.show_frame(Page1))
+                             command=lambda: page_1())
 
-        var = tk.StringVar(self, "1")
+        var2 = tk.IntVar(app, 1)
 
         # Dictionary to create multiple buttons
-        values = {"Remove Windows & migrate my Library (Music, Photos, Videos) to Linux": "1",
-                  "Nuke Windows & all data(!) and start fresh with Linux": "2",
-                  "Do nothing and let me partition later (Dual boot possible)": "3"}
+
+        values = {lang_windows_option1: 1,
+                  lang_windows_option2: 2,
+                  lang_windows_option3: 3}
 
         # Loop is used to create multiple Radiobuttons
         # rather than creating each button separately
         for (text, value) in values.items():
-            ttk.Radiobutton(middle_frame, text=text, variable=var,
+            ttk.Radiobutton(middle_frame, text=text, variable=var2,
                             value=value).pack(anchor="w", ipady=5)
 
         button1.pack(anchor="se", side="right", ipadx=15, padx=10)
         button2.pack(anchor="se", side="right", padx=5)
 
+    page_check()
+    app.mainloop()
 
-win_width, win_height = 550, 400
 
-# Driver Code
-app = TkinterApp()
-app.title("Lnixify")
-app.geometry("800x500")
-# app.wm_minsize(700, 400)
-# app.wm_maxsize(1200, 800)
-# app.iconbitmap("yourimage.ico")
-app.resizable(False, False)
-app.tk.call('tk', 'scaling', 2.0)
-app.mainloop()
+if __name__ == '__main__':
+    main()
