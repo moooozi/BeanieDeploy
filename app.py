9da92a5426a62a46d90e053befbe2952597b1799
DEVmozi <zid.m@pm.me>
2022-03-21 13:00:34 +0100

    connected with IDE

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 # from APP_INFO import *
+import pathlib
 import tkinter as tk
 from tkinter import ttk
 from multiprocessing import Process, Queue
-
 import APP_INFO
-from lang_en import *
+import translations.en as lang
 from functions import *
 
 # Driver Code
 app = tk.Tk()
-app.title(SW_NAME)
+app.title(APP_INFO.SW_NAME)
 app.geometry("800x500")
 # Style
 style = ttk.Style(app)
-app.tk.call('source', 'C:/Users/trapp/PycharmProjects/Lnixify/azure dark/azure dark.tcl')
+app.tk.call('source', str(pathlib.Path(__file__).parent) + '/azure dark/azure dark.tcl')
 style.theme_use('azure')
 style.configure("Accentbutton", foreground='white')
 style.configure("Togglebutton", foreground='white')
@@ -25,17 +25,59 @@ LARGEFONT = ("Ariel", 20)
 MEDIUMFONT = ("Ariel", 16)
 SMALLFONT = ("Ariel", 12)
 
+w_var = 'w'
+ne_var = 'ne'
+se_var = 'se'
+nw_var = 'nw'
+left_var = 'left'
+right_var = 'right'
+
+
+def right_to_left_lang(var):
+    global w_var, nw_var, ne_var, right_var, left_var, right_var, se_var
+    if var:
+        if ne_var == 'ne':
+            w_var = 'e'
+            ne_var = 'nw'
+            se_var = 'sw'
+            nw_var = 'ne'
+            left_var = 'right'
+            right_var = 'left'
+            rebuild_container()
+
+    else:
+        if ne_var == 'nw':
+            w_var = 'w'
+            ne_var = 'ne'
+            se_var = 'se'
+            nw_var = 'nw'
+            left_var = 'left'
+            right_var = 'right'
+            rebuild_container()
+
+
 # creating a container
 container = tk.Frame(app)
 container.pack(side="top", fill="both", expand=True)
-container.grid_rowconfigure(0, weight=1)
-container.grid_columnconfigure(0, weight=1)
 # Frames
-top_frame = tk.Frame(container, height=100, bg='yellow')
-top_frame.pack(fill="x", ipadx=10, ipady=10)
-left_frame = tk.Frame(container, width=200, bg='blue')
-left_frame.pack(fill="y", side="left", ipadx=10, ipady=10)
-middle_frame = tk.Frame(container, bg='cyan', height=800)
+top_frame = tk.Frame(container, height=100)
+left_frame = tk.Frame(container, width=200)
+left_frame_img = tk.PhotoImage(file='resources/leftframe.png')
+left_frame_label = tk.Label(left_frame, image=left_frame_img)
+top_frame_img = tk.PhotoImage(file='resources/topframe.png')
+top_frame_label = tk.Label(top_frame, image=top_frame_img)
+
+
+def rebuild_container():
+    top_frame.pack(fill="x")
+    left_frame.pack(fill="y", side=left_var)
+    left_frame_label.pack()
+    top_frame_label.pack()
+
+
+rebuild_container()
+
+middle_frame = tk.Frame(container, height=800)
 middle_frame.pack(fill="both", expand=1, padx=20, pady=20)
 
 
@@ -44,15 +86,33 @@ def clear_frame():
         widgets.destroy()
 
 
+lang_available = ["English", "Deutsch", "Arabic"]
+
+
+def change_lang(new_lang_code, pagename):
+    global lang
+    if new_lang_code == 'English':
+        import translations.en as lang
+        right_to_left_lang(0)
+    if new_lang_code == 'Deutsch':
+        import translations.de as lang
+        right_to_left_lang(0)
+    if new_lang_code == 'Arabic':
+        import translations.ar as lang
+        right_to_left_lang(1)
+
+    pagename()
+
+
 # page_check
 def main():
     def page_check():
         page_name = "page_check"
 
-        label2 = ttk.Label(middle_frame, text=lang_check_running, font=MEDIUMFONT)
+        label2 = ttk.Label(middle_frame, text=lang.ln_check_running, font=MEDIUMFONT)
         progressbar_check = ttk.Progressbar(middle_frame, orient='horizontal', length=500, mode='indeterminate')
 
-        label2.pack(pady=40, anchor="w")
+        label2.pack(pady=40, anchor=w_var)
         progressbar_check.pack(expand=True)
 
         progressbar_check.start(10)
@@ -64,7 +124,7 @@ def main():
             app.update()
         compatibility_results = queue1.get()
 
-        button2 = ttk.Button(middle_frame, text=lang_btn_quit,
+        button2 = ttk.Button(middle_frame, text=lang.ln_btn_quit,
                              command=lambda: app.destroy())
 
         print(compatibility_results)
@@ -75,116 +135,130 @@ def main():
 
             label2.pack_forget()
             progressbar_check.pack_forget()
-            label4 = ttk.Label(middle_frame, text=lang_error_title, font=MEDIUMFONT)
+            label4 = ttk.Label(middle_frame, text=lang.ln_error_title, font=MEDIUMFONT)
             errors = []
             if compatibility_results['result_uefi_check'] == 0:
-                errors.append(lang_error_uefi_0)
+                errors.append(lang.ln_error_uefi_0)
             if compatibility_results['result_uefi_check'] == 9:
-                errors.append(lang_error_uefi_9)
+                errors.append(lang.ln_error_uefi_9)
             if compatibility_results['result_totalram_check'] == 0:
-                errors.append(lang_error_totalram_0)
+                errors.append(lang.ln_error_totalram_0)
             if compatibility_results['result_totalram_check'] == 9:
-                errors.append(lang_error_totalram_9)
+                errors.append(lang.ln_error_totalram_9)
             if compatibility_results['result_space_check'] == 0:
-                errors.append(lang_error_space_0)
+                errors.append(lang.ln_error_space_0)
             if compatibility_results['result_space_check'] == 9:
-                errors.append(lang_error_space_9)
+                errors.append(lang.ln_error_space_9)
             if compatibility_results['result_resizable_check'] == 9:
-                errors.append(lang_error_resizable_0)
+                errors.append(lang.ln_error_resizable_0)
             if compatibility_results['result_resizable_check'] == 0:
-                errors.append(lang_error_resizable_9)
+                errors.append(lang.ln_error_resizable_9)
 
             label5 = ttk.Label(middle_frame, text=("\n".join(errors)), font=SMALLFONT)
 
-            label4.pack(pady=40, anchor="nw")
-            label5.pack(padx=10, anchor="w")
-            button2.pack(anchor="se", side="right", ipadx=15, padx=10)
+            label4.pack(pady=40, anchor=nw_var)
+            label5.pack(padx=10, anchor=w_var)
+            button2.pack(anchor=se_var, side=right_var, ipadx=15, padx=10)
 
     # page_1
     def page_1(space_check_results):
-        page_name = "page_1"
+        def page_name(): page_1(space_check_results)
+
         clear_frame()
 
-        label2 = ttk.Label(middle_frame, text=lang_install_question, font=MEDIUMFONT)
-        label2.pack(pady=40, anchor="w")
-        button1 = ttk.Button(middle_frame, text=lang_btn_next, style="Accentbutton",
+        label2 = ttk.Label(middle_frame, text=lang.ln_install_question, font=MEDIUMFONT)
+        label2.pack(pady=40, anchor=w_var)
+        button1 = ttk.Button(middle_frame, text=lang.ln_btn_next, style="Accentbutton",
                              command=lambda: page_2(space_check_results, var1))
+        lang_var = tk.StringVar()
+        lang_list = ttk.Combobox(middle_frame, textvariable=lang_var)
+        lang_list['values'] = lang_available
+        lang_list['state'] = 'readonly'
+        lang_list.pack()
+
+        def change_callback(*args):
+            change_lang(lang_var.get(), page_name)
+
+        lang_list.bind('<<ComboboxSelected>>', change_callback)
+
         # putting the button in its place by
         # using grid
         var1 = tk.IntVar(app, 1)
         var1.set(1)
 
-        r1_install = ttk.Radiobutton(middle_frame, text=lang_install_options[1], variable=var1, value=1)
-        r1_install.pack(anchor="w", ipady=5)
-        r2_install = ttk.Radiobutton(middle_frame, text=lang_install_options[2], variable=var1, value=2)
-        r2_install.pack(anchor="w", ipady=5)
-        r3_install = ttk.Radiobutton(middle_frame, text=lang_install_options[3], variable=var1, value=3)
-        r3_install.pack(anchor="w", ipady=5)
+        r1_install = ttk.Radiobutton(middle_frame, text=lang.ln_install_options[1], variable=var1, value=1)
+        r1_install.pack(anchor=w_var, ipady=5)
+        r2_install = ttk.Radiobutton(middle_frame, text=lang.ln_install_options[2], variable=var1, value=2)
+        r2_install.pack(anchor=w_var, ipady=5)
+        r3_install = ttk.Radiobutton(middle_frame, text=lang.ln_install_options[3], variable=var1, value=3)
+        r3_install.pack(anchor=w_var, ipady=5)
 
-        button1.pack(anchor="se", side="right", ipadx=15, padx=10)
+        button1.pack(anchor=se_var, side=right_var, ipadx=15, padx=10)
+        lang_list.pack(anchor=ne_var, side=right_var)
 
     # page_2
     def page_2(space_check_results, selection1):
         page_name = "page_2"
         clear_frame()
-        label2 = ttk.Label(middle_frame, text=lang_windows_question, font=MEDIUMFONT)
-        label2.pack(pady=40, anchor="w")
+        label2 = ttk.Label(middle_frame, text=lang.ln_windows_question, font=MEDIUMFONT)
+        label2.pack(pady=40, anchor=w_var)
 
-        button1 = ttk.Button(middle_frame, text=lang_btn_next, style="Accentbutton",
-                             command=lambda: page_1(space_check_results))
-
-        button2 = ttk.Button(middle_frame, text="Back",
+        button1 = ttk.Button(middle_frame, text=lang.ln_btn_next, style="Accentbutton",
                              command=lambda: page_verify(space_check_results, selection1, var2))
+
+        button2 = ttk.Button(middle_frame, text=lang.ln_btn_back,
+                             command=lambda: page_1(space_check_results))
 
         var2 = tk.IntVar(app, 1)
         if space_check_results == 2:
-            r1_windows = ttk.Radiobutton(middle_frame, text=lang_windows_options[1], variable=var2, value=1)
+            r1_windows = ttk.Radiobutton(middle_frame, text=lang.ln_windows_options[1], variable=var2, value=1)
         else:
-            r1_windows = ttk.Radiobutton(middle_frame, text=lang_windows_option1_disabled, state='disabled')
-        r2_windows = ttk.Radiobutton(middle_frame, text=lang_windows_options[2], variable=var2, value=2)
-        r3_windows = ttk.Radiobutton(middle_frame, text=lang_windows_options[3], variable=var2, value=3)
-        r4_windows = ttk.Radiobutton(middle_frame, text=lang_windows_options[4], variable=var2, value=3)
+            r1_windows = ttk.Radiobutton(middle_frame, text=lang.ln_windows_option1_disabled, state='disabled')
+        r2_windows = ttk.Radiobutton(middle_frame, text=lang.ln_windows_options[2], variable=var2, value=2)
+        r3_windows = ttk.Radiobutton(middle_frame, text=lang.ln_windows_options[3], variable=var2, value=3)
+        r4_windows = ttk.Radiobutton(middle_frame, text=lang.ln_windows_options[4], variable=var2, value=4)
 
-        r1_windows.pack(anchor="w", ipady=5)
-        r2_windows.pack(anchor="w", ipady=5)
-        r3_windows.pack(anchor="w", ipady=5)
-        r4_windows.pack(anchor="w", ipady=5)
+        r1_windows.pack(anchor=w_var, ipady=5)
+        r2_windows.pack(anchor=w_var, ipady=5)
+        r3_windows.pack(anchor=w_var, ipady=5)
+        r4_windows.pack(anchor=w_var, ipady=5)
 
-        button1.pack(anchor="se", side="right", ipadx=15, padx=10)
-        button2.pack(anchor="se", side="right", padx=5)
+        button1.pack(anchor=se_var, side=right_var, ipadx=15, padx=10)
+        button2.pack(anchor=se_var, side=right_var, padx=5)
 
     def page_verify(space_check_results, selection1, selection2):
         page_name = "page_2"
         clear_frame()
-        label2 = ttk.Label(middle_frame, text=lang_verify_question, font=MEDIUMFONT)
-        label2.pack(pady=40, anchor="w")
+        label2 = ttk.Label(middle_frame, text=lang.ln_verify_question, font=MEDIUMFONT)
+        label2.pack(pady=40, anchor=w_var)
 
-        button1 = ttk.Button(middle_frame, text=lang_btn_start, style="Accentbutton",
+        button1 = ttk.Button(middle_frame, text=lang.ln_btn_start, style="Accentbutton",
                              command=lambda: page_installing(selection1, selection2))
 
-        button2 = ttk.Button(middle_frame, text="Back",
+        button2 = ttk.Button(middle_frame, text=lang.ln_btn_back,
                              command=lambda: page_1(space_check_results))
 
-        review_text1 = 'x  ' + lang_install_options[selection1] + '\n' + 'x  ' + lang_windows_options[selection2]
+        review_text1 = 'x  ' + lang.ln_install_options[selection1] + '\n' + 'x  ' + lang.ln_windows_options[
+            selection2]
         review_text = ttk.Label(middle_frame, text=review_text1)
 
         review_text.pack(anchor='w', padx=5)
 
-        button1.pack(anchor="se", side="right", ipadx=15, padx=10)
-        button2.pack(anchor="se", side="right", padx=5)
+        button1.pack(anchor=se_var, side=right_var, ipadx=15, padx=10)
+        button2.pack(anchor=se_var, side=right_var, padx=5)
 
     def page_installing(selection1, selection2):
         page_name = "page_installing"
         clear_frame()
         queue2 = Queue()
-        label2 = ttk.Label(middle_frame, text=lang_install_running, font=MEDIUMFONT)
+        label2 = ttk.Label(middle_frame, text=lang.ln_install_running, font=MEDIUMFONT)
         progressbar_install = ttk.Progressbar(middle_frame, orient='horizontal', length=500, mode='determinate')
         current_job_title = "Starting download"
         current_job = ttk.Label(middle_frame, text=current_job_title, font=MEDIUMFONT)
 
-        label2.pack(pady=40, anchor="w")
+        label2.pack(pady=40, anchor=w_var)
         progressbar_install.pack(expand=True)
-        current_job.pack(pady=40, anchor="w")
+        current_job.pack(pady=40, anchor=w_var)
 
         download_path = get_user_home_dir() + '\win2linux_tmpdir'
         install_media_path = download_path + "\install_media.iso"
@@ -199,8 +273,8 @@ def main():
 
         def retrack(jobid, totalsize):
             downloaded = track(jobid)
-            percent = (downloaded * 100)/totalsize
-            progressbar_install['value'] = percent*0.85
+            percent = (downloaded * 100) / totalsize
+            progressbar_install['value'] = percent * 0.85
             app.update()
             if downloaded < totalsize:
                 app.after(1000, retrack(jobid, totalsize))
@@ -208,7 +282,7 @@ def main():
         retrack(job_id, download_size)
         finish_downloaded(job_id)
 
-    page_check()
+    page_1(1)
     app.mainloop()
 
 
---
+++ translations/ar.py
@@ -0,0 +1,37 @@
+import APP_INFO
+
+ln_btn_next = "التالي"
+ln_btn_back = "السابق"
+ln_btn_quit = "إنهاء"
+ln_btn_start = "ابدأ"
+
+ln_check_running = "جاري التحقق من متطلبات النظام. الرجاء الانتظار ..."
+ln_install_running = "جارٍ التثبيت ..."
+
+ln_error_title = "لا يمكن تشغيل% s على نظامك" % APP_INFO.SW_NAME
+ln_error_uefi_0 = "نظامك لا يدعم (أو لا يستخدم) تمهيد UEFI."
+ln_error_uefi_9 = "تعذر التحقق من دعم تمهيد UEFI."
+ln_error_totalram_0 = "لا يحتوي نظامك على سعة ذاكرة RAM كافية."
+ln_error_totalram_9 = "فشل التحقق من سعة ذاكرة الوصول العشوائي المتاحة."
+ln_error_space_0 = "لا توجد مساحة كافية على محرك أقراص النظام. حرر بعض المساحة وحاول مرة أخرى لاحقًا"
+ln_error_space_9 = "فشل التحقق من مساحة القرص المتوفرة على محرك أقراص النظام."
+ln_error_resizable_0 = "فشل التحقق من تغيير حجم محرك أقراص النظام."
+ln_error_resizable_9 = "لا توجد مساحة تقلص كافية على محرك أقراص النظام."
+
+ln_install_question = "؟%s كيف تريد تثبيت" % APP_INFO.distro_name
+
+ln_install_options = [0, "KDE تثبيت سريع مع سطح مكتب",
+                      "GNOME التثبيت السريع مع سطح مكتب",
+                      "متقدم: دعني أقوم بتهيئة تطبيقاتي لاحقًا"]
+
+ln_windows_question = "بياناتك؟ Windows حسنًا , ماذا عن "
+
+ln_windows_options = [0, "Windows بجانب %s تثبيت" % APP_INFO.distro_name,
+                      " %s وترحيل مكتبتي (الموسيقى والصور ومقاطع الفيديو) إلى  Windows إزالة" % APP_INFO.distro_name,
+                      " %s وجميع البيانات وابدأ من جديد باستخدام Windows إزالة" % APP_INFO.distro_name,
+                      "خيارات متقدمة: لا تفعل شيئًا ودعني أقسم لاحقًا"]
+ln_windows_option1_disabled = "% s (مساحة غير كافية)" % ln_windows_options[1]
+
+ln_verify_question = "هذا ما سيتم القيام به. انقر فوق% s مرة واحدة جاهزًا" % ln_btn_start
+
+ln_job_downloading_install_media = "تنزيل وسائط التثبيت"
