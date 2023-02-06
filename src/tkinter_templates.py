057bb1cdee3b5c277714be1877290cd5bd4d7228
DEVmozi <zid.m@pm.me>
2023-02-06 02:30:09 +0100

    New theme: Sun-Valley-ttk-theme (style files needed) Dark Mode changes for improved consistency

--- src/tkinter_templates.py
+++ src/tkinter_templates.py
@@ -3,6 +3,8 @@ import tkinter.ttk as ttk
 import ctypes
 from gui_functions import detect_darkmode_in_windows
 import globals as GV
+import resources.style.theme.svtheme.sv_ttk as sv_ttk
+
 WIDTH = 750
 HEIGHT = 500
 MINWIDTH = 650
@@ -13,7 +15,7 @@ WIDTH_OFFSET = 400
 HEIGHT_OFFSET = 400
 TOP_FRAME_HEIGHT = 80
 LEFT_FRAME_WIDTH = 0
-COLOR_MODE = 'dark' if detect_darkmode_in_windows() else 'light'
+DARK_MODE = detect_darkmode_in_windows()
 
 FONTS_large = ("Ariel", 24)
 FONTS_medium = ("Ariel Bold", 16)
@@ -21,53 +23,57 @@ FONTS_small = ("Ariel", 13)
 FONTS_smaller = ("Ariel", 12)
 FONTS_tiny = ("Ariel", 11)
 
-tkinter_background_color = '#856ff8'
-color_red = '#e81123'
-color_blue = '#0067b8'
-top_background_color = '#e6e6e6'
+tkinter_background_color = ''
+color_red = ''
+color_blue = ''
+top_background_color = ''
 
 
-def apply_theme(theme, tkinter):
+def dark_decorations(ye_or_nah, tkinter):
+    # force Windows black borders for Windows 11
+    tkinter.update()
+    set_window_attribute = ctypes.windll.dwmapi.DwmSetWindowAttribute
+    get_parent = ctypes.windll.user32.GetParent
+    hwnd = get_parent(tkinter.winfo_id())
+    rendering_policy = 20 if ye_or_nah else 0
+    value = 2
+    value = ctypes.c_int(value)
+    set_window_attribute(hwnd, rendering_policy, ctypes.byref(value),
+                         ctypes.sizeof(value))
+
+
+def dark_theme(ye_or_nah, tkinter):
     global tkinter_background_color, color_red, color_blue, top_background_color, left_background_color
-    if theme == 'light':
+    if not ye_or_nah:
+        color_mode = 'light'
         tkinter_background_color = '#856ff8'
         color_red = '#e81123'
         color_blue = '#0067b8'
         top_background_color = '#e6e6e6'
-    elif theme == 'dark':
+    else:
+        color_mode = 'dark'
         tkinter_background_color = '#856ff8'
         color_red = '#ff4a4a'
         color_blue = '#3aa9ff'
         top_background_color = '#2b2b2b'
-        # force Windows black borders for Windows 11
-        tkinter.update()
-        set_window_attribute = ctypes.windll.dwmapi.DwmSetWindowAttribute
-        get_parent = ctypes.windll.user32.GetParent
-        hwnd = get_parent(tkinter.winfo_id())
-        rendering_policy = 20
-        value = 2
-        value = ctypes.c_int(value)
-        set_window_attribute(hwnd, rendering_policy, ctypes.byref(value),
-                             ctypes.sizeof(value))
-    tkinter.tk.call("set_theme", theme)
+    dark_decorations(ye_or_nah, tkinter)
+    sv_ttk.set_theme(color_mode)
     tkinter.update()
 
 
 def init_tkinter(title, icon=None):
     tkinter = tk.Tk()
     tkinter.title(title)
-
-    #windll.shcore.SetProcessDpiAwareness(1)
-    dpi_factor = ctypes.windll.user32.GetDpiForSystem()/96
+    # windll.shcore.SetProcessDpiAwareness(1)
+    dpi_factor = ctypes.windll.user32.GetDpiForSystem() / 96
 
     tkinter.geometry(str("%sx%s+%s+%s" % (WIDTH, HEIGHT, WIDTH_OFFSET, HEIGHT_OFFSET)))
     tkinter.minsize(MINWIDTH, MINHEIGHT)
     tkinter.maxsize(int(MAXWIDTH * dpi_factor), int(MAXHEIGHT * dpi_factor))
     tkinter.iconbitmap(icon)
-    tkinter.tk.call("source", GV.PATH.CURRENT_DIR + '/resources/style/theme/azure.tcl')
-    tkinter.tk.call('tk', 'scaling', 1.4)
-    apply_theme(COLOR_MODE, tkinter)
-    #tkinter.resizable(False, False)
+    # tkinter.tk.call('tk', 'scaling', 1.4)
+    dark_theme(DARK_MODE, tkinter)
+    # tkinter.resizable(False, False)
 
     return tkinter
 
@@ -80,12 +86,14 @@ def build_main_gui_frames(parent, left_frame_img_path=None, top_frame_height=TOP
     """
     top_frame = tk.Frame(parent, height=top_frame_height, width=MINWIDTH, background=top_background_color)
     left_frame = tk.Frame(parent, width=left_frame_width, height=MINHEIGHT)
-    mid_frame = tk.Frame(parent, height=MINHEIGHT - top_frame_height,)
+    mid_frame = tk.Frame(parent, height=MINHEIGHT - top_frame_height, )
 
     top_frame.pack(fill="x", side='top')
     top_frame.pack_propagate(False)
-    #left_frame.pack(fill="y", side=GV.UI.DI_VAR['l'])
-    #left_frame.pack_propagate(False)
+    # dark_mode_var = tk.BooleanVar(parent, DARK_MODE)
+    # add_check_btn(top_frame, "Dark Mode", dark_mode_var, lambda *args: dark_theme(dark_mode_var.get(), parent), switch=True)
+    # left_frame.pack(fill="y", side=GV.UI.DI_VAR['l'])
+    # left_frame.pack_propagate(False)
     mid_frame.pack(fill="both", expand=True, padx=20, pady=(20, 20))
     mid_frame.pack_propagate(False)
     return top_frame, mid_frame, left_frame
@@ -108,7 +116,7 @@ def generic_page_layout(parent, title=None, primary_btn_txt=None, primary_btn_co
     return frame
 
 
-def open_popup(parent, x_size: int = None, y_size: int = None,):
+def open_popup(parent, x_size: int = None, y_size: int = None, ):
     """
     Pops up window to get input from user and freezes the main GUI while waiting for response
     :param y_size: window height in pixels
@@ -123,10 +131,11 @@ def open_popup(parent, x_size: int = None, y_size: int = None,):
     x_app_size = parent.winfo_width()
     y_app_size = parent.winfo_height()
     #  position the pop-up window at the center of its parent
-    geometry = "%dx%d+%d+%d" % (x_size, y_size, x_position + (x_app_size - x_size) / 2 + 20, y_position + (y_app_size - y_size) / 2 + 20)
+    geometry = "%dx%d+%d+%d" % (
+    x_size, y_size, x_position + (x_app_size - x_size) / 2 + 20, y_position + (y_app_size - y_size) / 2 + 20)
     pop.geometry(geometry)
-    apply_theme(COLOR_MODE, pop)
-    #pop.protocol("WM_DELETE_WINDOW", False)
+    dark_theme(DARK_MODE, pop)
+    # pop.protocol("WM_DELETE_WINDOW", False)
     pop.focus_set()
     pop.grab_set()
     border_frame.pack(expand=1, fill='both', pady=5, padx=5)
@@ -136,14 +145,14 @@ def open_popup(parent, x_size: int = None, y_size: int = None,):
     return pop, pop_frame
 
 
-def input_pop_up(parent, title_txt=None, msg_txt=None, primary_btn_str=None, secondary_btn_str=None,):
+def input_pop_up(parent, title_txt=None, msg_txt=None, primary_btn_str=None, secondary_btn_str=None, ):
     x_size = 600
     y_size = int(len(msg_txt) / 2.8 + 180 + 13 * msg_txt.count('\n'))
     pop, pop_frame = open_popup(parent, x_size, y_size)
     pop_var = tk.IntVar(pop)
     msg_font = FONTS_smaller
     layout_frame = generic_page_layout(pop_frame, title_txt, primary_btn_str, lambda *args: validate_pop_input(1),
-                        secondary_btn_str, lambda *args: validate_pop_input(0), title_pady=5)
+                                       secondary_btn_str, lambda *args: validate_pop_input(0), title_pady=5)
     if msg_txt:
         add_text_label(layout_frame, msg_txt, msg_font, pady=10)
 
@@ -178,13 +187,13 @@ def add_secondary_btn(parent, text, command):
 def add_multi_radio_buttons(parent, items: dict, var, validate_func=None):
     frame = add_frame_container(parent, fill='both')
     for index, item in enumerate(items.keys()):
-        button = add_radio_btn(frame, items[item]["name"], var, item, command=lambda: validate_func(),pack=False)
+        button = add_radio_btn(frame, items[item]["name"], var, item, command=lambda: validate_func(), pack=False)
         button.grid(ipady=5, row=index, column=0, sticky=GV.UI.DI_VAR['nw'])
         if "error" in items[item] and items[item]["error"]:
             button.configure(state='disabled')
             ttk.Label(frame, wraplength=GV.UI.width, justify="center", text=items[item]["error"],
-                      font=FONTS_smaller,foreground=color_red).grid(ipadx=5, row=index, column=1,
-                                                                    sticky=GV.UI.DI_VAR['w'])
+                      font=FONTS_smaller, foreground=color_red).grid(ipadx=5, row=index, column=1,
+                                                                     sticky=GV.UI.DI_VAR['w'])
         elif "description" in items[item] and items[item]["description"]:
             ttk.Label(frame, wraplength=GV.UI.width, justify="center", text=items[item]["description"],
                       font=FONTS_tiny, foreground=color_blue).grid(ipadx=5, row=index, column=1,
@@ -195,12 +204,12 @@ def add_multi_radio_buttons(parent, items: dict, var, validate_func=None):
 
 def add_page_title(parent, text, pady=(0, 15)):
     title = ttk.Label(parent, wraplength=GV.UI.width, justify=GV.UI.DI_VAR['l'], text=text, font=FONTS_medium)
-    title.pack(pady=pady, padx=0, fill='x',)
+    title.pack(pady=pady, padx=0, fill='x', )
     return title
 
 
 def add_radio_btn(parent, text, var, value, command=None, is_disabled=None, ipady=5, side=None, pack=True):
-    radio = ttk.Radiobutton(parent, text=text, variable=var, value=value)
+    radio = ttk.Radiobutton(parent, text=text, variable=var, value=value, width=99)
     if pack: radio.pack(ipady=ipady, side=side)
     if command: radio.configure(command=command)
     if is_disabled: radio.configure(state='disabled')
@@ -208,7 +217,7 @@ def add_radio_btn(parent, text, var, value, command=None, is_disabled=None, ipad
 
 
 def add_check_btn(parent, text, var, command=None, is_disabled=None, pady=5, pack=True, switch=False):
-    check = ttk.Checkbutton(parent, text=text, variable=var, onvalue=True, offvalue=False)
+    check = ttk.Checkbutton(parent, text=text, variable=var, onvalue=True, offvalue=False, width=99)
     if switch: check.configure(style='Switch.TCheckbutton')
     if pack: check.pack(ipady=5, pady=pady)
     if command: check.configure(command=command)
@@ -225,7 +234,8 @@ def add_text_label(parent, text=None, font=FONTS_small, var=None, pady=20, padx=
     if (not var and text) or (var and not text):
         label = ttk.Label(parent, wraplength=GV.UI.width, justify=GV.UI.DI_VAR['l'],
                           text=text, textvariable=var, foreground=foreground, font=font)
-    else: return -1
+    else:
+        return -1
     if pack: label.pack(pady=pady, padx=padx, anchor=anchor)
     return label
 
@@ -246,9 +256,9 @@ def add_frame_container(parent, fill='both'):
     return frame
 
 
-def add_progress_bar(parent,):
+def add_progress_bar(parent, ):
     progressbar = ttk.Progressbar(parent, orient='horizontal', length=MAXWIDTH, mode='determinate')
-    progressbar.pack(pady=(0,20))
+    progressbar.pack(pady=(0, 20))
     return progressbar
 
 
@@ -278,5 +288,3 @@ def var_tracer(var, mode, cb):
     for tracer in tracers_list:
         var.trace_remove(*tracer)
     var.trace_add(mode=mode, callback=cb)
-
-
