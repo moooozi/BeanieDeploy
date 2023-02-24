959e559b543203e8605d43fb7e4b82ae1c13c522
DEVmozi <zid.m@pm.me>
2023-02-24 02:02:30 +0100

    minor improvements

--- src/page_autoinst_addition_2.py
+++ src/page_autoinst_addition_2.py
@@ -35,7 +35,7 @@ def run(app):
     timezone_all = sorted(autoinst.all_timezones())
     lists_frame = ttk.Frame(frame_radios)
     timezone_txt = ttk.Label(lists_frame, wraplength=GV.UI.width, justify=GV.UI.DI_VAR['l'], text=LN.list_timezones,
-                             font=tkt.FONTS_smaller)
+                             font=tkt.FONTS_tiny)
     timezone_list = ttk.Combobox(lists_frame, name="timezone", textvariable=tk_var.custom_timezone_var)
     timezone_list['values'] = tuple(timezone_all)
     timezone_list['state'] = 'readonly'
@@ -43,7 +43,7 @@ def run(app):
     all_keymaps = autoinst.get_available_keymaps()
 
     keyboards_txt = ttk.Label(lists_frame, wraplength=GV.UI.width, justify=GV.UI.DI_VAR['l'], text=LN.list_keymaps,
-                              font=tkt.FONTS_smaller)
+                              font=tkt.FONTS_tiny)
     keyboard_list = ttk.Combobox(lists_frame, name="keyboard", textvariable=tk_var.custom_keymap_var)
     keyboard_list['values'] = tuple(all_keymaps)
     keyboard_list['state'] = 'readonly'
@@ -53,13 +53,13 @@ def run(app):
 
     def spawn_more_widgets(*args):
         if tk_var.keymap_timezone_source_var.get() == 'custom':
-            lists_frame.grid(ipady=5, padx=30, row=3, column=0, sticky=GV.UI.DI_VAR['nw'])
-            keyboards_txt.grid(pady=5, padx=5, column=0, row=1, sticky=GV.UI.DI_VAR['nw'])
+            lists_frame.grid(ipady=5, padx=30, row=3, column=0, sticky=GV.UI.DI_VAR['w'])
+            keyboards_txt.grid(pady=5, padx=5, column=0, row=1, sticky=GV.UI.DI_VAR['w'])
             keyboard_list.grid(pady=5, padx=5, column=1, row=1)
-            timezone_txt.grid(pady=5, padx=5, column=0, row=0, sticky=GV.UI.DI_VAR['nw'])
+            timezone_txt.grid(pady=5, padx=5, column=0, row=0, sticky=GV.UI.DI_VAR['w'])
             timezone_list.grid(pady=5, padx=5, column=1, row=0)
         else:
-            lists_frame.pack_forget()
+            lists_frame.grid_forget()
 
     def next_btn_action(*args):
         page_autoinst_addition_3.run(app)
