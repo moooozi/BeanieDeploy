2767765e19aac22332d613e473682f260f8c37d0
Mo Zidane <zid.m@pm.me>
2023-11-23 09:07:00 +0100

    make-winrar-sfx script calls compile_and_collect

--- make-stanalone/make-winrar-sfx.bat
+++ make-stanalone/make-winrar-sfx.bat
@@ -14,7 +14,10 @@ rem Create the SFX archive name using the app name and version
 set SFXName=%AppName%-v%AppVersion%-Bundled-Win64.exe
 rem Check if file exists
 if exist "%OUTPUTDIR%\%SFXName%" del "%OUTPUTDIR%\%SFXName%"
+rem Run compile collect script
+python "./compile_and_collect.py"
 rem Run the WinRAR command to create the SFX archive
-"%WINRAREXE%" a -sfx -s -md32m -m3 -iicon"%PROJECTDIR%\src\resources\style\app-icon.ico" -k -z"winrar_sfx_options.txt" "%OUTPUTDIR%\%SFXName%" "%PROJECTDIR%\interpreter" "%PROJECTDIR%\src" "%PROJECTDIR%\ReleaseInfo.txt"
+copy "%PROJECTDIR%\ReleaseInfo.txt" "%PROJECTDIR%\release\ReleaseInfo.txt"
+"%WINRAREXE%" a -sfx -s -md32m -m3 -iicon"%PROJECTDIR%\src\resources\style\app-icon.ico" -k -z"winrar_sfx_options.txt" "%OUTPUTDIR%\%SFXName%" "%PROJECTDIR%\release"
 rem Display a message when the SFX archive is created
 echo The SFX archive %SFXName% has been created successfully.
