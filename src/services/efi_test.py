6463fedfd4914b207b990b4ab66f6846e111a05f
DEVmozi <zid.m@pm.me>
2025-07-27 18:12:35 -1100

    test efi

--- src/services/efi_test.py
+++ src/services/efi_test.py
@@ -1,4 +1,5 @@
 from ctypes import WinError
+from math import e, radians
 import os
 import sys
 import uuid
@@ -12,22 +13,74 @@ def _setup_sys_path():
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src')))
 
 
+def set_stuff():
+    print("Create a new entry")
+    new_entry = fwvars.get_parsed_boot_entry(8)
+    new_entry.description = "Fedora Installer"
+    for path in new_entry.file_path_list.paths:
+        if path.is_hard_drive():
+            hd_node = path.get_hard_drive_node()
+            if not hd_node: continue
+            """
+            hd_node.partition_guid = "AC27B345-EC75-4731-A73C-97EDAE7CB6DE".lower()
+            hd_node.partition_number = 4
+            hd_node.partition_start_lba = 200478720
+            hd_node.partition_size_lba = 7643136
+            hd_node.partition_signature = uuid.UUID(hd_node.partition_guid).bytes_le
+            path.set_hard_drive_node(hd_node)
+            """
+        elif path.is_file_path():
+            path.set_file_path("\\EFI\\FedoraRecovery\\BOOTX64.EFI")
+        else: continue
+    # Delete the optional data bytes from the entry
+    #new_entry.optional_data = b''
+    # Find an unused entry_id for the new entry
+    new_entry_id = None
+    for i in range(50):
+        try:
+            fwvars.get_boot_entry(i)
+        except OSError:
+            new_entry_id = i
+            break
+    new_entry_id = 16
+    fwvars.set_parsed_boot_entry(new_entry_id, new_entry)
+    print(f"New entry created with ID {new_entry_id}")
+    print(fwvars.get_parsed_boot_entry(new_entry_id).description)
+    print("Set as BootNext")
+    fwvars.set_boot_next(new_entry_id)
+    print("Add to the boot order as second option")
+    boot_order = [8]
+    boot_order.insert(1, new_entry_id)
+    fwvars.set_boot_order(boot_order)
+    print(f"BootOrder updated: {fwvars.get_boot_order()}")
+
+    
+
 if __name__ == "__main__":
     _setup_sys_path()
     from services.system import get_admin
     get_admin()
     with fwvars.adjust_privileges():
+        set_stuff()
+        windows_entry = None
+        #fwvars.set_boot_order([8])
+        print("Current Boot Order:", fwvars.get_boot_order())
+        entries = {i: fwvars.get_parsed_boot_entry(i).description for i in fwvars.get_boot_order()}
+        for index, entry_desc in entries.items():
+            if "fedora" in entry_desc.lower() and index != 14:
+                pass
+                #fwvars.set_boot_entry(index, b'')
         entries = fwvars.get_boot_order()
         bootnext = fwvars.get_boot_next()
         #fwvars.set_boot_next(8)
         print("BootNext:", bootnext)
-        print("Boot Next Description:", fwvars.get_parsed_boot_entry(bootnext).description)
         for entry in entries:
+            print(f"Boot Entry ID: {entry}")
             if not fwvars.get_parsed_boot_entry(entry).file_path_list.get_hard_drive_node():
-                continue
-            print(f"Boot Entry: {entry}")
+                pass
             entry = fwvars.get_parsed_boot_entry(entry)
-            print("  Description:", entry.description)
+            print(f"Entry Description: {entry.description}")
+            print(f"Boot Entry: {entry}")
             print("  File Path List:")
             for path in entry.file_path_list.paths:
                 print("    -", path)
@@ -44,47 +97,16 @@ if __name__ == "__main__":
                 print("    - Signature Type:", hard_drive_node.signature_type)
             print("  Attributes:", entry.attributes)
             print("  Optional Data:", entry.optional_data)
+        print("Setting a boot entry from Windows' entry")
+        # Create dict of entry_id -> description
+        entries = {i: fwvars.get_parsed_boot_entry(i).description for i in fwvars.get_boot_order()}
+        for index, entry_desc in entries.items():
+            if "windows boot manager" in entry_desc.lower():
+                print(f"Found Windows Boot Manager at index {index}")
+                windows_entry = index
+                break
+        raise SystemExit
 
-            print("Setting a boot entry from Windows' entry")
-            # Create dict of entry_id -> description
-            entries = {i: fwvars.get_parsed_boot_entry(i).description for i in fwvars.get_boot_order()}
-            windows_entry = None
-            for index, entry_desc in entries.items():
-                if "windows boot manager" in entry_desc.lower():
-                    print(f"Found Windows Boot Manager at index {index}")
-                    windows_entry = index
-                    break
-            print("Create a new entry")
-            new_entry = fwvars.get_parsed_boot_entry(windows_entry)
-            new_entry.description = "Fedora Installer"
-            for path in new_entry.file_path_list.paths:
-                if path.is_hard_drive():
-                    hd_node = path.get_hard_drive_node()
-                    if not hd_node: continue
-                    hd_node.partition_guid = "AC27B345-EC75-4731-A73C-97EDAE7CB6DE".lower()
-                    hd_node.partition_number = 4
-                    hd_node.partition_start_lba = 200478720
-                    hd_node.partition_size_lba = 7643136
-                    hd_node.partition_signature = uuid.UUID(hd_node.partition_guid).bytes_le
-                elif path.is_file_path():
-                    path.set_file_path(r"\EFI\BOOT\bootx64.efi")
-                else: continue
-            # Delete the optional data bytes from the entry
-            new_entry.optional_data = b''
-            # Find an unused entry_id for the new entry
-            new_entry_id = None
-            for i in range(50):
-                try:
-                    fwvars.get_boot_entry(i)
-                except OSError:
-                    new_entry_id = i
-                    break
-            fwvars.set_parsed_boot_entry(new_entry_id, new_entry)
-            print(f"New entry created with ID {new_entry_id}")
-            print(fwvars.get_parsed_boot_entry(new_entry_id).description)
-            print("Set as BootNext")
-            fwvars.set_boot_next(new_entry_id)
-            print(f"BootNext set to {new_entry_id}")
 
 def get_partition_guids():
     cmd = [
